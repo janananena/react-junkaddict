37d86690caeec9fbda1af0f051769a87bac08c13
jana <janananena@gmail.com>
2024-09-27 01:37:47 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useEffect, useState} from "react";
-import {getPrograms} from "./apihandler/DbApiHandlers";
+import {addProgramm, getPrograms} from "./apihandler/DbApiHandlers";
 
 function JunkCard({program}) {
 
@@ -15,7 +15,8 @@ function JunkCard({program}) {
         : <h1>
             {program.title}
         </h1>;
-    const station = <a href={program.link}><img src={require(`./logos/${program.station.toLowerCase()}.jpg`)} alt={program.station} width="50"/></a>;
+    const station = <a href={program.link}><img src={require(`./logos/${program.station.toLowerCase()}.jpg`)}
+                                                alt={program.station} width="50"/></a>;
     const time = <div>{program.day.capitalize} {program.time}</div>;
     const category = <div>{program.category.capitalize}</div>;
 
@@ -33,9 +34,9 @@ function JunkCard({program}) {
     </table>);
 }
 
-function sortTimes(time1, time2) {
-    const t1 = time1.split(':').map((t) => parseInt(t, 10));
-    const t2 = time2.split(':').map((t) => parseInt(t, 10));
+function sortTimes(program1, program2) {
+    const t1 = program1.time.split(':').map((t) => parseInt(t, 10));
+    const t2 = program2.time.split(':').map((t) => parseInt(t, 10));
     return t1[0] === t2[0] ? t1[1] - t2[1] : t1[0] - t2[0];
 }
 
@@ -46,15 +47,63 @@ function JunkDay({programs}) {
     </ul>);
 }
 
-function JunkTable({programs}) {
+function JunkTable({programs, setPrograms}) {
+    const [add, setAdd] = useState(false);
     const daygroups = {"mo": [], "di": [], "mi": [], "do": [], "fr": [], "sa": [], "so": []};
     programs.map((p) => daygroups[p.day].push(p));
 
     const headers = (Object.keys(daygroups)).map((k) => <td key={k}>{k}</td>);
     const body = (Object.entries(daygroups)).map(([k, v]) => <td key={k + v.title}><JunkDay programs={v}/></td>)
+
+    const toggleAddForm = () => setAdd(!add);
+    async function handleAdd(event){
+        event.preventDefault();
+        const program = await addProgramm(event.currentTarget);
+        setPrograms([...programs, program]);
+        toggleAddForm();
+    }
+    const addForm = <form onSubmit={handleAdd}>
+        <fieldset>
+            <label htmlFor="title"> Offizieller Name
+                <input id="title" type="text" name="title" required/>
+            </label>
+            <label form="nick"> Nick
+                <input id="nick" type="text" name="nick"/>
+            </label>
+            <label form="station"> Sender
+                <input id="station" type="text" name="station" required/>
+            </label>
+            <label form="link"> Link
+                <input id="link" type="url" name="link" required/>
+            </label>
+            <label form="day"> Tag
+                <select id="day" name="day" required>
+                    <option key="mo" value="mo">Mo</option>
+                    <option key="di" value="di">Di</option>
+                    <option key="mi" value="mi">Mi</option>
+                    <option key="do" value="do">Do</option>
+                    <option key="fr" value="fr">Fr</option>
+                    <option key="sa" value="sa">Sa</option>
+                    <option key="so" value="so">So</option>
+                </select>
+            </label>
+            <label form="time"> Uhrzeit (hh:mm)
+                <input id="time" type="time" name="time" step="any" required/>
+            </label>
+            <label form="category"> Kategorie
+                <select id="category" name="category" required>
+                    <option value="tv">Video</option>
+                    <option value="podcast">Podcast</option>
+                </select>
+            </label>
+            <label form="addbutton">
+                <button id="addbutton" type="submit">add</button>
+            </label>
+        </fieldset>
+    </form>;
     return (
         <table>
-            <thead>
+        <thead>
             <tr>{headers}</tr>
             </thead>
             <tbody>
@@ -62,6 +111,18 @@ function JunkTable({programs}) {
                 {body}
             </tr>
             </tbody>
+            <tfoot>
+            <tr>
+                <td>
+                    <button onClick={toggleAddForm}>ADD</button>
+                </td>
+            </tr>
+            {add && <tr>
+                <td>
+                    {addForm}
+                </td>
+            </tr>}
+            </tfoot>
         </table>);
 }
 
@@ -78,7 +139,7 @@ function App() {
     }, [])
 
     return (
-        <JunkTable programs={programs}/>);
+        <JunkTable programs={programs} setPrograms={setPrograms}/>);
 }
 
 export default App;
--- src/apihandler/DbApiHandlers.jsx
+++ src/apihandler/DbApiHandlers.jsx
@@ -6,3 +6,9 @@ export async function getPrograms() {
     let response = await axios.get(`${BaseUrl}/programs`);
     return response.data;
 }
+
+export async function addProgramm(program) {
+    let headers = {headers: {'Content-Type': 'application/json'}};
+    let response = await axios.post(`${BaseUrl}/programs/`, program, headers);
+    return response.data;
+}
