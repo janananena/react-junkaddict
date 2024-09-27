851bba9035227428dd6b7f7e40e2d0d650a80b77
jana <janananena@gmail.com>
2024-09-27 15:13:27 +0200

    edit junk

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import './App.css';
 import {useEffect, useState} from "react";
 import {addProgram, changeProgram, getPrograms} from "./apihandler/DbApiHandlers";
+import _ from "lodash";
 
 function JunkCard({program, onAir, setProgram}) {
     const [editOnAir, setEditOnAir] = useState(false);
+    const [editProgram, setEditProgram] = useState(false);
 
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
-    const name = program.nick ?
-        <div>
-            <h1>
-                {program.nick}
-            </h1>
-            <h2>
-                {program.title}
-            </h2></div>
-        : <h1>
+    const name = program.nick ? <div>
+        <h1>
+            {program.nick}
+        </h1>
+        <h2>
             {program.title}
-        </h1>;
-    const station = <a href={program.link}><img src={require(`./logos/${program.station.toLowerCase()}.jpg`)}
-                                                alt={program.station} width="50"/></a>;
+        </h2></div> : <h1>
+        {program.title}
+    </h1>;
+    const station = <a href={program.link}>
+        <img src={require(`./logos/${program.station.toLowerCase()}.jpg`)} alt={program.station} width="50"/>
+    </a>;
     const time = <div>{capitalizeFirstLetter(program.day)} {program.time}</div>;
     const category = <div>{capitalizeFirstLetter(program.category)}</div>;
 
@@ -40,20 +41,46 @@ function JunkCard({program, onAir, setProgram}) {
             currentSeason: true
         });
         setProgram(prog);
+        toggleEditOnAir();
+    }
+
+    async function handleEditProgram(event) {
+        event.preventDefault();
+        const input = event.currentTarget;
+        const newProg = {...program};
+        if(input.nick.value !== ""){
+            newProg.nick = input.nick.value;
+        }
+        newProg.title = input.title.value;
+        newProg.station = input.station.value;
+        newProg.day = input.day.value;
+        newProg.time = input.time.value;
+        newProg.category = input.category.value;
+        console.log(`isEqual is ${_.isEqual(newProg, program)}`);
+        if (!_.isEqual(newProg, program)) {
+            const prog = await changeProgram(newProg);
+            setProgram(prog);
+        }
+        toggleEditProgram();
+    }
+
+    function toggleEditOnAir() {
+
         setEditOnAir(!editOnAir);
     }
 
-    const offAirButton = <button onClick={handleOffAir}>Staffel Ende</button>;
+    function toggleEditProgram() {
 
-    function toggleEditOnAir() {
-        setEditOnAir(!editOnAir);
+        setEditProgram(!editProgram);
     }
 
     const onAirButton = <button onClick={toggleEditOnAir}>Neue Staffel</button>;
+    const offAirButton = <button onClick={handleOffAir}>Staffel Ende</button>;
+    const editProgramButton = <button onClick={toggleEditProgram}>EDIT</button>;
 
     const changeDayTimeForm = <form onSubmit={handleOnAir}>
         <fieldset>
-            <label form="day"> Tag
+            <label htmlFor="day"> Tag
                 <select id="day" name="day" defaultValue={program.day} required>
                     <option key="mo" value="mo">Mo</option>
                     <option key="di" value="di">Di</option>
@@ -64,7 +91,7 @@ function JunkCard({program, onAir, setProgram}) {
                     <option key="so" value="so">So</option>
                 </select>
             </label>
-            <label form="time"> Uhrzeit (hh:mm)
+            <label htmlFor="time"> Uhrzeit (hh:mm)
                 <input id="time" type="time" name="time" step="any" defaultValue={program.time} required/>
             </label>
             <button id="cancelChange" onClick={toggleEditOnAir}>cancel</button>
@@ -72,7 +99,53 @@ function JunkCard({program, onAir, setProgram}) {
         </fieldset>
     </form>;
 
-    return (<table>
+    const editJunkCard = <form onSubmit={handleEditProgram}>
+        <table>
+            <tbody>
+            <tr>
+                <td>
+                    <fieldset>
+                        <input id="nick" type="text" name="nick" placeholder="Nick" defaultValue={program.nick}/>
+                        <input id="title" type="text" name="title" defaultValue={program.title} required/>
+                    </fieldset>
+                </td>
+                <td><input id="station" type="text" name="station" defaultValue={program.station} required/></td>
+            </tr>
+            <tr>
+                <td>
+                    <select id="day" name="day" defaultValue={program.day} required>
+                        <option key="mo" value="mo">Mo</option>
+                        <option key="di" value="di">Di</option>
+                        <option key="mi" value="mi">Mi</option>
+                        <option key="do" value="do">Do</option>
+                        <option key="fr" value="fr">Fr</option>
+                        <option key="sa" value="sa">Sa</option>
+                        <option key="so" value="so">So</option>
+                    </select>
+                    <input id="time" type="time" name="time" step="any" defaultValue={program.time} required/>
+                </td>
+                <td>
+                    <select id="category" name="category" defaultValue={program.category} required>
+                        <option value="tv">Video</option>
+                        <option value="podcast">Podcast</option>
+                    </select>
+                </td>
+            </tr>
+            </tbody>
+            <tfoot>
+            <tr>
+                <td>
+                    <button id="cancelChange" onClick={toggleEditProgram}>cancel</button>
+                </td>
+                <td>
+                    <button id="submitChange" type="submit">save</button>
+                </td>
+            </tr>
+            </tfoot>
+        </table>
+    </form>;
+
+    const displayJunkTable = <table>
         <tbody>
         <tr>
             <td>{name}</td>
@@ -85,12 +158,20 @@ function JunkCard({program, onAir, setProgram}) {
         </tbody>
         <tfoot>
         <tr>
-            {onAir && <td colSpan="2">{offAirButton}</td>}
+            {onAir && <>
+                <td>{editProgramButton}</td>
+                <td>{offAirButton}</td>
+            </>}
             {!onAir && !editOnAir && <td colSpan="2">{onAirButton}</td>}
             {!onAir && editOnAir && <td colSpan="2">{changeDayTimeForm}</td>}
         </tr>
         </tfoot>
-    </table>);
+    </table>;
+
+    return (<>
+        {!editProgram && displayJunkTable}
+        {editProgram && editJunkCard}
+    </>);
 }
 
 function sortTimes(program1, program2) {
@@ -139,16 +220,16 @@ function JunkTable({programs, setPrograms}) {
             <label htmlFor="title"> Offizieller Name
                 <input id="title" type="text" name="title" required/>
             </label>
-            <label form="nick"> Nick
+            <label htmlFor="nick"> Nick
                 <input id="nick" type="text" name="nick"/>
             </label>
-            <label form="station"> Sender
+            <label htmlFor="station"> Sender
                 <input id="station" type="text" name="station" required/>
             </label>
-            <label form="link"> Link
+            <label htmlFor="link"> Link
                 <input id="link" type="url" name="link" required/>
             </label>
-            <label form="day"> Tag
+            <label htmlFor="day"> Tag
                 <select id="day" name="day" required>
                     <option key="mo" value="mo">Mo</option>
                     <option key="di" value="di">Di</option>
@@ -159,45 +240,42 @@ function JunkTable({programs, setPrograms}) {
                     <option key="so" value="so">So</option>
                 </select>
             </label>
-            <label form="time"> Uhrzeit (hh:mm)
+            <label htmlFor="time"> Uhrzeit (hh:mm)
                 <input id="time" type="time" name="time" step="any" required/>
             </label>
-            <label form="category"> Kategorie
+            <label htmlFor="category"> Kategorie
                 <select id="category" name="category" required>
                     <option value="tv">Video</option>
                     <option value="podcast">Podcast</option>
                 </select>
             </label>
             <input id="currentSeason" type="hidden" name="currentSeason" value="true"/>
-            <label form="addbutton">
-                <button id="addbutton" type="submit">add</button>
-            </label>
+            <button id="addbutton" type="submit">add</button>
         </fieldset>
     </form>;
-    return (
-        <table>
-            <caption>Reality Stundenplan</caption>
-            <thead>
-            <tr>{headers}</tr>
-            </thead>
-            <tbody>
-            <tr>
-                {body}
-            </tr>
-            </tbody>
-            <tfoot>
-            <tr>
-                <td colSpan="7">
-                    <button onClick={toggleAddForm}>ADD</button>
-                </td>
-            </tr>
-            {add && <tr>
-                <td colSpan="7">
-                    {addForm}
-                </td>
-            </tr>}
-            </tfoot>
-        </table>);
+    return (<table>
+        <caption>Reality Stundenplan</caption>
+        <thead>
+        <tr>{headers}</tr>
+        </thead>
+        <tbody>
+        <tr>
+            {body}
+        </tr>
+        </tbody>
+        <tfoot>
+        <tr>
+            <td colSpan="7">
+                <button onClick={toggleAddForm}>ADD</button>
+            </td>
+        </tr>
+        {add && <tr>
+            <td colSpan="7">
+                {addForm}
+            </td>
+        </tr>}
+        </tfoot>
+    </table>);
 }
 
 function App() {
@@ -212,8 +290,7 @@ function App() {
         getProgramList();
     }, [])
 
-    return (
-        <JunkTable programs={programs} setPrograms={setPrograms}/>);
+    return (<JunkTable programs={programs} setPrograms={setPrograms}/>);
 }
 
 export default App;
