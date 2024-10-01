823cbb29b34b199962f6839fd6c1f7cb59bfbc42
jana <janananena@gmail.com>
2024-10-01 22:28:39 +0200

    move icons

--- src/components/junk/JunkTableCard.jsx
+++ src/components/junk/JunkTableCard.jsx
@@ -4,6 +4,7 @@ import AddJunkCardForm from "./cards/forms/AddJunkCardForm";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import {addIcon} from "../../data/JunkIcons";
 
 function JunkTableCard({programs, setPrograms}) {
     const [addProgram, setAddProgram] = useState(false);
@@ -56,10 +57,7 @@ function JunkTableCard({programs, setPrograms}) {
             </Card.Body>
             <Card.Body>
                 <Button type="button" variant="outline-secondary" onClick={toggleAddForm}>
-                    <svg className="d-inline-block align-content-top bi bi-plus-square" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                    </svg>
+                    {addIcon}
                     {' '}
                     Add
                 </Button>
