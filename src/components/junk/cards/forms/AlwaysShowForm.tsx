4784f2614ad2071dcba015ae13c1aab39fa8ed34
jana <janananena@gmail.com>
2024-10-21 20:51:47 +0200

    smaller always show

--- src/components/junk/cards/forms/AlwaysShowForm.tsx
+++ src/components/junk/cards/forms/AlwaysShowForm.tsx
@@ -12,12 +12,12 @@ function AlwaysShowForm() {
         <>
             <ListGroup>
                 {notes.map((note, i) =>
-                    <Form.Control type="input" key={`form-input-${i}`} name={`form-input-${i}`} value={note.note} readOnly/>
+                    <Form.Control type="input" key={`form-input-${i}`} name={`form-input-${i}`} value={note.note} size="sm" readOnly/>
                 )}
             </ListGroup>
             <ListGroup>
                 {links.map((link, i) =>
-                    <ListGroup.Item action key={`form-link-${i}`} onClick={() => window.open(link.junklink, "_blank")}>
+                    <ListGroup.Item action key={`form-link-${i}`} onClick={() => window.open(link.junklink, "_blank")} style={{fontSize:'14px', padding:'4px 8px 4px 8px'}}>
                         {link.junklink}
                     </ListGroup.Item>
                 )}
